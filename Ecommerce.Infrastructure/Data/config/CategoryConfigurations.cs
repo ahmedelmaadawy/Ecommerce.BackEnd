28155062635ffa603cabbc7250987ccf853cdfce
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2025-04-25 14:34:49 +0300

    seeding data

--- Ecommerce.Infrastructure/Data/config/CategoryConfigurations.cs
+++ Ecommerce.Infrastructure/Data/config/CategoryConfigurations.cs
@@ -10,6 +10,15 @@ namespace Ecommerce.Infrastructure.Data.config
         {
             builder.Property(x => x.Name).IsRequired().HasMaxLength(30);
             builder.Property(x => x.Id).IsRequired();
+            builder.HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Description = "Test",
+                }
+
+                );
         }
     }
 }
--- Ecommerce.Infrastructure/Data/config/ProductConfigurations.cs
+++ Ecommerce.Infrastructure/Data/config/ProductConfigurations.cs
@@ -11,6 +11,16 @@ namespace Ecommerce.Infrastructure.Data.config
             builder.Property(x => x.Name).IsRequired();
             builder.Property(x => x.Description).IsRequired();
             builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Description = "Test",
+                    Price = 12,
+                    CategoryId = 1
+                }
+                );
         }
     }
 }
